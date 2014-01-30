7286e65537eb35f09f20c096c793c5b1c131e940
sanntidT19 <yngveska@stud.ntnu.no>
2014-01-30 19:37:48 +0100

    Update UDP.go
    I did some at home
    
    Olav

--- UDP.go
+++ UDP.go
@@ -2,46 +2,18 @@ package main
 
 import (
 "net";
-"bufio";
-"fmt";
-."time"
+"fmt"
+"time"
 )
 
 func main() {
-
-	raddr, err := ResolveUDPAddr("udp", "129.241.187.255:20019")
-
-	c ,err :=  net.DialUDP("udp","129.241.187.157:20019")
-	if err != nil {
-     	    fmt.Printf("unable to connect to server, code red: %s\n", err.Error())
-     	}
+	var buf [1024]byte
 	
-	
-	
+	addr, err := net.ResolveUDPAddr("udp", "129.241.187.255:20019")//UDP is requred to broadcast and listen to the same port(?)
+	sock, err := net.ListenUDP("udp", "129.241.187.157:20019")
 	for {
+	    rlen, remote, err := sock.ReadFromUDP(buf)
 		
-		Echo(raddr)
-		Broadcast(c)
-		Sleep(100*Millisecond)
-	}
-	c.Close()
-	raddr.Close()
+		fmt.Fprint(rlen)
+		sleep(100*Millieseconds)			    
 }
-
-func Echo(raddr net.UDPConn) {
-	line, err := bufio.NewReader(raddr).ReadString('\x00')
-	if err != nil {
-		fmt.Printf("Failure to read: %s\n",err.Error())	
-		return
-	}
-	fmt.Println("Received: ", line)
-}
-
-func Broadcast(c net.UDPConn) {
-	_, err = c.WriteToUDP([]byte("Hello yo yo hello\x00"))
-	if err != nil {
-		fmt.Printf("Failure to write: %s\n", err.Error())
-		return	
-	}
-}
-	// defer c.Close() //Closes the TCP connection so you dont abuse it, defer is called when the function returns.
