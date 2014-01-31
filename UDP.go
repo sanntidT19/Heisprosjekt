4e915e3c59bfb7918fd932bece932ca0e00fbd95
student <student@Sanntid.(none)>
2014-01-31 14:22:00 +0100

    work 31.jan

--- UDP.go
+++ UDP.go
@@ -3,17 +3,55 @@ package main
 import (
 "net";
 "fmt"
-"time"
+."time"
 )
 
-func main() {
-	var buf [1024]byte
-	
-	addr, err := net.ResolveUDPAddr("udp", "129.241.187.255:20019")//UDP is requred to broadcast and listen to the same port(?)
-	sock, err := net.ListenUDP("udp", "129.241.187.157:20019")
+/*type UDPAddr() struct{
+IP IP
+Port int
+
+}*/
+
+func write(sock2 *net.UDPConn) {
 	for {
-	    rlen, remote, err := sock.ReadFromUDP(buf)
-		
-		fmt.Fprint(rlen)
-		sleep(100*Millieseconds)			    
+		sock2.Write([]byte("Hello\n"))
+		Sleep(100*Millisecond)
+	}
+}
+
+func read(sock *net.UDPConn){
+	for {
+		buf := make([]byte, 1024)		
+		rlen, raddr, _ := sock.ReadFromUDP(buf)
+		//fmt.Printf("This is length: %s\n %s\n",rlen,raddr)
+		if raddr.IP.String() != "129.241.187.157" {
+			fmt.Println(string(buf))
+		}
+	_ = rlen
+	_ = raddr
+	}
+}
+
+func main() {
+	address, err := net.ResolveUDPAddr("udp", "129.241.187.255:20019")//UDP is requred to broadcast and listen to the same port(?)
+	if err != nil {
+		fmt.Printf("code red")	
+	}
+	sock2, err := net.DialUDP("udp", nil, address)
+	
+	if err != nil {
+			fmt.Printf("code cyan")
+		}
+
+	sock, err := net.ListenUDP("udp", address)
+	if err != nil {
+			fmt.Printf("code blue")	
+		}
+	
+	go write(sock2)
+	go read(sock)
+
+	a := make(chan string)
+	<-a
+
 }
